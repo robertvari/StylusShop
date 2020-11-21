9c06d231afbce6b79089e3a5f140ffcbea5e2ad3
Róbert Vári <mail.robertvari@gmail.com>
2020-11-21 17:37:30 +0100

    Image slider API fetch

--- stylus_gui/src/components/home/ImageSlider.js
+++ stylus_gui/src/components/home/ImageSlider.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 function ImageSlider(props) {
+    const [items, set_items] = useState([])
+
+    const fetch_items = () => {
+        axios({
+            method: "get",
+            url: "http://localhost:3001/slideshow"
+        })
+            .then(res => set_items(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() =>{
+        fetch_items()
+    }, [])
+
     return (
         <div className="image-slider-container">
-            Image Show
+            {
+                items.map((item, index) =>
+                    <div style={{backgroundImage: `url(${item.image})`}}>
+                        {item.message}
+                    </div>
+                )
+            }
         </div>
     );
 }
