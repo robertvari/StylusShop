ecf244da50754f9e7dff8489e285878c4ea334dd
Róbert Vári <mail.robertvari@gmail.com>
2020-11-21 16:51:28 +0100

    Home with API request

--- stylus_gui/src/components/Home.js
+++ stylus_gui/src/components/Home.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios"
 
 function Home(props) {
+    const [posts, set_posts] = useState([])
+
+    const fetch_items = () => {
+        axios({
+            method: "get",
+            url: "http://localhost:3001/home"
+        })
+            .then(res => set_posts(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() =>{
+        fetch_items()
+    }, [])
+
     return (
-        <h1>Home</h1>
+        <div>
+            {
+              posts.map(item => <h1>{item.title}</h1>)
+            }
+        </div>
     );
 }
 
