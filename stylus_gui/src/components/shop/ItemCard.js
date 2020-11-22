0fe7c5d198557bd12ad3d3a62ee340896d1ff238
Róbert Vári <mail.robertvari@gmail.com>
2020-11-22 15:41:59 +0100

    Item Card

--- stylus_gui/src/components/shop/ItemCard.js
+++ stylus_gui/src/components/shop/ItemCard.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import {Intcomma} from "../../utilities";
+
 
 function ItemCard(props) {
+    const {title, image, price, discont, in_stock} = props.data
+
     return (
-        <div>Shop Item</div>
+        <div className="shop-item-card">
+            <div>
+                <img src={image} alt=""/>
+                <h4>{title}</h4>
+            </div>
+
+            <div>
+                {
+                    in_stock?
+                        <small style={{color: "green"}}>Raktáron!</small>
+                        :
+                        <small style={{color: "red"}}>Rendelésre</small>
+                }
+            </div>
+
+            <div className="price-container">
+                {discont&& <small className="discount-price">{Intcomma(discont)} Ft</small>}
+                <h2>{Intcomma(price)} Ft</h2>
+            </div>
+
+            <button>
+                <i className="fas fa-cart-arrow-down"/>
+                Kosárba!
+            </button>
+
+        </div>
     );
 }
 
