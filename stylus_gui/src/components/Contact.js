d2994ddcd522082e0a2898779b250fe1e1b22f37
Róbert Vári <mail.robertvari@gmail.com>
2020-11-22 16:35:34 +0100

    Contact component

--- stylus_gui/src/components/Contact.js
+++ stylus_gui/src/components/Contact.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Contact(props) {
+    const [email, set_email] = useState("")
+    const [message, set_message] = useState("")
+
+    const send_message = () => {
+        console.log(email, message)
+    }
+
     return (
-        <h1>Contact</h1>
+        <div className="form-container">
+            <h2>Kérdésed van?</h2>
+            <small>Írj nekünk és mi válaszolunk.</small>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={
+                    (e) => set_email(e.target.value)
+                }
+            />
+
+            <textarea
+                cols="30"
+                rows="10"
+                placeholder="Üzenet"
+                value={message}
+                onChange={
+                    (e) => set_message(e.target.value)
+                }
+            />
+            <hr/>
+            <button onClick={send_message}>Küldés</button>
+        </div>
     );
 }
 
