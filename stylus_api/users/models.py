7e6dfc02fa8d20d4c2dc95c2c856317e11a6fd56
Róbert Vári <mail.robertvari@gmail.com>
2020-12-05 15:40:26 +0100

    User manager: create_user

--- stylus_api/users/models.py
+++ stylus_api/users/models.py
@@ -1,9 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 import time
+
+
+class StylusUserManager(BaseUserManager):
+    def create_user(self, email, password=None, superuser=False):
+        if not email:
+            raise ValueError("User must have an email!")
+
+        if not password:
+            raise ValueError("User most have a password!")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+
+        if superuser:
+            user.is_admin = True
+            user.is_staff = True
+            user.is_superuser = True
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self):
+        pass
 
 
 class StylusUser(AbstractBaseUser, PermissionsMixin):
@@ -16,6 +44,8 @@ class StylusUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = StylusUserManager()
 
     def __str__(self):
         return self.email
