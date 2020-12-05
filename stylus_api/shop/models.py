c7583200d54e211b5d9d8c05314ff438d7d08cf5
Róbert Vári <mail.robertvari@gmail.com>
2020-12-05 18:11:54 +0100

    Shop categories and items model

--- stylus_api/shop/models.py
+++ stylus_api/shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+def image_dir_path(instance, filename):
+    return f"shop/{instance.category.name}/{slugify(instance.title)}/{filename}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class ShopItem(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    title = models.CharField(max_length=200)
+
+    # promotion card
+    promotion_image = models.ImageField(upload_to=image_dir_path, blank=True)
+    promotion_title = models.CharField(max_length=200, blank=True)
+    promoted = models.BooleanField(default=False)
+
+    # shop item card
+    card_image = models.ImageField(upload_to=image_dir_path)
+    card_title = models.CharField(max_length=200, blank=True)
+    price = models.IntegerField()
+    discount_price = models.IntegerField()
+
+    in_stock = models.BooleanField(default=True)
+
+    # details
+    description = models.TextField(max_length=5000, blank=True)
+    image1 = models.ImageField(upload_to=image_dir_path, blank=True)
+    image2 = models.ImageField(upload_to=image_dir_path, blank=True)
+    image3 = models.ImageField(upload_to=image_dir_path, blank=True)
