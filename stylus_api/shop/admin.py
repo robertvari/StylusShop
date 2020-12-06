e475bcf2160ab9efa9bbd17defa1c7315632b2c5
Róbert Vári <mail.robertvari@gmail.com>
2020-12-06 14:49:01 +0100

    ShopItemAdmin config

--- stylus_api/shop/admin.py
+++ stylus_api/shop/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import ShopItem, Category, Order, OrderItem
 
-admin.site.register(ShopItem)
+
+class ShopItemAdmin(admin.ModelAdmin):
+    list_display = ["title", "in_stock", "promoted", "price", "discount_price"]
+    list_editable = ["promoted", "price", "discount_price"]
+
+
+admin.site.register(ShopItem, ShopItemAdmin)
 admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(OrderItem)
