7e2e52a0049f0c8721468e50fceb2f0bacd145e9
Róbert Vári <mail.robertvari@gmail.com>
2020-12-06 18:13:49 +0100

    Local storage (not working)

--- stylus_gui/src/components/contexts/ShoppingCart.js
+++ stylus_gui/src/components/contexts/ShoppingCart.js
@@ -1,9 +1,21 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
 export const ShoppingCartContext = createContext(true)
 
 export const ShoppingCartProvider = (props) => {
     const [shopping_list, set_shopping_list] = useState([])
+
+    useEffect(() => {
+        const new_cart_list = JSON.stringify(shopping_list)
+        localStorage.setItem("shopping_list", new_cart_list)
+
+        console.log(localStorage.getItem("shopping_list"))
+    }, [shopping_list])
+
+    useEffect(() => {
+        const old_cart_list = localStorage.getItem("shopping_list")
+        set_shopping_list(JSON.parse(old_cart_list))
+    }, [])
 
     return (
         <ShoppingCartContext.Provider value={{
