f5286864d9983c6e0b6864a78f6b786d632eff9a
Róbert Vári <mail.robertvari@gmail.com>
2020-12-20 14:40:31 +0100

    API: admin customization for orders

--- stylus_api/shop/admin.py
+++ stylus_api/shop/admin.py
@@ -5,10 +5,18 @@ from .models import ShopItem, Category, Order, OrderItem
 
 class ShopItemAdmin(admin.ModelAdmin):
     list_display = ["title", "in_stock", "promoted", "price", "discount_price"]
-    list_editable = ["promoted", "price", "discount_price"]
+    list_editable = ["promoted", "in_stock", "price", "discount_price"]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "email", "phone"]
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["item", "quantity"]
 
 
 admin.site.register(ShopItem, ShopItemAdmin)
 admin.site.register(Category)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
